2c3d13f917e2f3f31e1b004fde624ef367640ac9
Min-Seok Kim <106315809+snumin44@users.noreply.github.com>
2024-08-10 05:06:06 +0900

    Update train.py

--- Seq2Seq/train/train.py
+++ Seq2Seq/train/train.py
@@ -168,10 +168,6 @@ def train(model, train_dataloader, optimizer, scheduler, args, scaler):
             train_loss = model(encoder_input_ids, encoder_attention_mask, decoder_input_ids, decoder_attention_mask, label)
                 
         total_train_loss += train_loss.mean()
-
-        # Clip the norm of the gradients to 1.0.
-        # This is to help prevent the "exploding gradients" problem.
-        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=5.0)   
                 
         if args.amp:
             scaler.scale(train_loss.mean()).backward()
